c83c2875b825490a7183d0ec35731b00965a031e
Svetlana Malashenko <svetlana.malashenko@logx.ru>
2024-11-11 19:09:38 +0300

    implemented functions for creating client and user options

---
+++ src/utils/searchOptions.js
@@ -0,0 +1,31 @@
+const createOptionsForClients = ({ logins, essence }) => {
+	return logins.reduce((acc, item) => {
+		const id = item[essence].id
+		const name = item[essence].name
+
+		const existingItem = acc.find(i => i.id === id)
+		if (!existingItem) {
+			acc.push({ id, name })
+		}
+
+		return acc
+	}, [])
+}
+// console.log(createOptionsForClients({ logins, essence: 'client' }))
+
+const createOptionsForUsers = ({ logins, id, essence }) => {
+	let options = []
+
+	logins.forEach(item => {
+		if (item.client.id === id) {
+			options.push({
+				label: item[essence].id,
+				name: item[essence].username,
+			})
+		}
+	})
+
+	return options
+}
+
+// console.log(createOptionsForUsers({ logins, id: 38, essence: 'user' }))
